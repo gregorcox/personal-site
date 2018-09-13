7dfae06197755f0ec4960fd56ca2c199c58d6c5c
Gregor Cox <gregorcox@gmail.com>
2018-09-13 17:56:05 +0100

    adds nav bar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-    <li>
-      <Link to="/projects">Projects</Link>
-    </li>
-    <li>
-      <Link to="/contact">Contact</Link>
-    </li>
-  </ul>
+  <div clasName="navbar-header">
+    <ul className="navbar">
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/projects">Projects</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li className="website-title">
+        <Link to="/">Gregor Cox</Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navbar;
