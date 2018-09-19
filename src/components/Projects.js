4abb711473fcea985314ba16630ed82051a30307
Gregor Cox <gregorcox@gmail.com>
2018-09-19 14:22:02 +0100

    adds keywords for SEO

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => (
       <div className="project-paragraph">
         <h2>Event Finder - JavaScript</h2>
         <h4 className="github"><a href="github.com/gregorcox/Event-Finder-Project">GitHub</a></h4>
-        <p>At CodeClan, my final project involved working as a group of four to create an single-page informational web application using vanilla JavaScript. Together we built Eventfull, a full-stack JavaScript app that allows users to search for events based on category, dates and location. <br/><br/>
+        <p>At CodeClan, my final project involved collaborating as an agile team of four to create an single-page informational web application using vanilla JavaScript. Together we built Eventfull, a full-stack JavaScript app that allows users to search for events based on category, dates and location. <br/><br/>
         The app uses two APIs (<a href="https://www.skiddle.com/api/">Skiddle</a> and <a href="https://geocode.xyz/">GeoCode</a>) to fetch event and geographical data. Search results are presented on an interactive Leaflet map where users can click through to see more information and buy tickets. The user can also save their favourite events through the use of a MongoDB NoSQL database. JavaScript is used heavily on the front-end to create a modern looking, responsive and dynamic UI.<br/><br/>
         In addition to learning a huge amount about JavaScript, we also had the opportunity to practise agile methodologies to plan and coordinate as a team. Stand ups, sprints, scrum boards and pair programming were all used to deliver a fully functional MVP with additional features before the deadline.</p>
 
