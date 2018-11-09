224211b7d67be8d150e1df2945075a6e8efb33e2
Gregor Cox <gregor@fxdigital.uk>
2018-11-09 18:19:36 +0000

    change about section paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => (
       
       Before moving to London I graduated from <a href="https://codeclan.com/">CodeClan's</a> software development course in Edinburgh. Throughout the 16 week course I developed strong programming skills in Ruby, Java, and JavaScript with a particular focus on web development. The course involved around 800 hours of class time and personal study, daily pair programming labs and three full-stack projects (you can check those out <a href="/projects" >here</a>). <br/><br/>
 
-      Before CodeClan I graduated with a 1st class BSc in Psychology, and it remains useful in my UX planning. Outside of software I also enjoy traveling to new places, football, film, and lifting heavy things.</p>
+      Prior to CodeClan I graduated with a 1st class BSc in Psychology, and it remains useful in my UX planning. Outside of software I also enjoy traveling to new places, football, film, and lifting heavy things.</p>
     </div>
     <div className="about-photo">
       <img className="profile" src={require('../images/profile.jpg')} alt="Gregor Cox" />
