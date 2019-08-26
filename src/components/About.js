057aaf8d632a44a609927db94f7dca26510388d5
Gregor Cox <gregor@fxdigital.uk>
2019-08-26 12:23:47 +0100

    use flex box

--- src/components/About.js
+++ src/components/About.js
@@ -4,20 +4,23 @@ import pdf from '../files/cv.pdf';
 const About = () => (
   <div className="about">
     <h1 className="headline">About me</h1>
-    <div className="about-paragraph">
-      <p><strong>I am passionate about software development and all things web. I love being able to turn complex problems into great looking user interfaces</strong>. <br/><br/>
-      
-      I am currently working as a front-end developer at a digital agency in Shoreditch where I use JavaScript to create modern looking, dynamic websites for a wide range of clients.<br/><br/> 
-      
-      Before moving to London I graduated from <a href="https://codeclan.com/">CodeClan's</a> software development course in Edinburgh. Throughout the 16 week course I developed strong programming skills in Ruby, Java, and JavaScript with a particular focus on web development. The course involved around 800 hours of class time and personal study, daily pair programming labs and three full-stack projects (you can check those out <a href="/projects" >here</a>). <br/><br/>
 
-      Prior to CodeClan I graduated with a 1st class BSc in Psychology, and it remains useful in my UX planning. Outside of software I also enjoy traveling to new places, football, film, and lifting heavy things.</p>
-    </div>
-    <div className="about-photo">
-      <img className="profile" src={require('../images/profile.jpg')} alt="Gregor Cox" />
-      {/* <form action={pdf} target="_blank">
-        <input className="button" type="submit" value="View my CV" />
-      </form> */}
+    <div className="about-container">
+      
+      <div className="about-paragraph">
+        <p><strong>I am passionate about software development and all things web. I love being able to turn complex problems into great looking user interfaces</strong>. <br/><br/>
+        
+        I am currently working as a front-end developer at a digital agency in Shoreditch where I use JavaScript to create modern looking, dynamic websites for a wide range of clients.<br/><br/> 
+        
+        Before moving to London I graduated from <a href="https://codeclan.com/">CodeClan's</a> software development course in Edinburgh. Throughout the 16 week course I developed strong programming skills in Ruby, Java, and JavaScript with a particular focus on web development. The course involved around 800 hours of class time and personal study, daily pair programming labs and three full-stack projects (you can check those out <a href="/projects" >here</a>). <br/><br/>
+
+        Prior to CodeClan I graduated with a 1st class BSc in Psychology, and it remains useful in my UX planning. Outside of software I also enjoy traveling to new places, football, film, and lifting heavy things.</p>
+      </div>
+
+      <div className="about-photo">
+        <img className="profile" src={require('../images/profile.jpg')} alt="Gregor Cox" />
+      </div>
+
     </div>
   </div>
 );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,23 +8,27 @@ const Projects = () => (
 
     <div className="project-info">
 
+      <div className="project-photo">
+        <img className="screenshot" src={require('../images/event_finder1.png')} alt="Event Finder" />
+        <img className="screenshot" src={require('../images/event_finder2.png')} alt="Event Finder" />
+      </div>
+
       <div className="project-paragraph">
         <h2>Event Finder - JavaScript</h2>
         <h4 className="github"><a href="https://www.github.com/gregorcox/Event-Finder-Project">GitHub</a></h4>
         <p>At CodeClan, my final project involved collaborating as an agile team of four to create an single-page informational web application using vanilla JavaScript. Together we built Eventfull, a full-stack JavaScript app that allows users to search for events based on category, dates and location. <br/><br/>
         The app uses two APIs (<a href="https://www.skiddle.com/api/">Skiddle</a> and <a href="https://geocode.xyz/">GeoCode</a>) to fetch event and geographical data. Search results are presented on an interactive Leaflet map where users can click through to see more information and buy tickets. The user can also save their favourite events through the use of a MongoDB NoSQL database. JavaScript is used heavily on the front-end to create a modern looking, responsive and dynamic UI.<br/><br/>
         In addition to learning a huge amount about JavaScript, we also had the opportunity to practise agile methodologies to plan and coordinate as a team. Stand ups, sprints, scrum boards and pair programming were all used to deliver a fully functional MVP with additional features before the deadline.</p>
-
-      </div>
-
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/event_finder1.png')} alt="Event Finder" />
-        <img className="screenshot" src={require('../images/event_finder2.png')} alt="Event Finder" />
       </div>
 
     </div>
 
     <div className="project-info">
+
+      <div className="project-photo">
+        <img className="screenshot" src={require('../images/spending1.png')} alt="Spending Tracker" />
+        <img className="screenshot" src={require('../images/spending2.png')} alt="Event Finder" />
+      </div>
 
       <div className="project-paragraph">
         <h2>Spending Tracker - Ruby</h2>
@@ -33,16 +37,14 @@ const Projects = () => (
         The project was created using Ruby with the Sinatra web framework. A postgreSQL database was also used to store transaction, merchant and tag data from the user. This meant users could also filter their transactions by merchant or tag. The app uses a sleek, minimalist front-end with a subtle colour pallette that results in a user friendly UI.</p>
       </div>
 
-
-
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/spending1.png')} alt="Spending Tracker" />
-        <img className="screenshot" src={require('../images/spending2.png')} alt="Event Finder" />
-      </div>
-
     </div>
 
     <div className="project-info">
+
+      <div className="project-photo">
+        <img className="screenshot" src={require('../images/news1.png')} alt="Spending Tracker" />
+        <img className="screenshot" src={require('../images/news2.png')} alt="Event Finder" />
+      </div>
 
       <div className="project-paragraph">
         <h2>News Website - Java</h2>
@@ -51,16 +53,8 @@ const Projects = () => (
         The app was built using Java and made use of the Spark web framework. A simple database using the Hibernate framework means creating, editing and deleting articles/journalists is quick and easy. New articles appear on the homepage where users can view the latest stories or use the navigation bar to browse articles based on their respective category. Test driven development was used throughout the project to ensure a reliable backend. The front-end uses a clean and user friendly UI that encourages users to make use of all areas of the app.</p>
       </div>
 
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/news1.png')} alt="Spending Tracker" />
-        <img className="screenshot" src={require('../images/news2.png')} alt="Event Finder" />
-      </div>
-
     </div>
-
-
-
-
+    
   </div>
 );
 
