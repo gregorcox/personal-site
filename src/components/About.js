2707ce0d55bafcb74a57f662c82276585e708966
Gregor Cox <gregor@fxdigital.uk>
2019-09-21 20:05:48 +0100

    remove unnecessary class name, update profile

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,11 @@ const About = () => (
     <h1 className="headline">About me</h1>
 
     <div className="about-container">
-      
+
       <div className="about-paragraph">
         <p><strong>I am passionate about software development and all things web. I love being able to turn complex problems into great looking user interfaces</strong>. <br/><br/>
         
-        I am currently working as a front-end developer at a digital agency in Shoreditch where I use JavaScript to create modern looking, dynamic websites for a wide range of clients.<br/><br/> 
+        I am currently working as a front-end developer at a digital agency in Shoreditch where I use JavaScript to build and maintain large scale web applications for a wide range of clients.<br/><br/> 
         
         Before moving to London I graduated from <a href="https://codeclan.com/">CodeClan's</a> software development course in Edinburgh. Throughout the 16 week course I developed strong programming skills in Ruby, Java, and JavaScript with a particular focus on web development. The course involved around 800 hours of class time and personal study, daily pair programming labs and three full-stack projects (you can check those out <a href="/projects" >here</a>). <br/><br/>
 
@@ -18,7 +18,7 @@ const About = () => (
       </div>
 
       <div className="about-photo">
-        <img className="profile" src={require('../images/profile.jpg')} alt="Gregor Cox" />
+        <img src={require('../images/profile.jpg')} alt="Gregor Cox" />
       </div>
 
     </div>
