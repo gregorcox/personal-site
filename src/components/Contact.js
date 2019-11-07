7652dde79c8b7b1b054c6b3a31ce6b63ea7687ee
Gregor Cox <gregor@fxdigital.uk>
2019-11-07 12:25:06 +0000

    add basic contact form html and css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,20 @@ import React from "react";
 const Contact = () => (
   <div className="contact">
     <h1>Contact me</h1>
-    <p className="email">To have a conversation, contact me at <a href= "tel:+447720595842">07720595842</a> or <a href="mailto:gregorcox@gmail.com?Subject=Hey%20Gregor!" target="_top"> gregorcox@gmail.com</a></p>
+    {/* <p className="email">To have a conversation, contact me at <a href= "tel:+447720595842">07720595842</a> or <a href="mailto:gregorcox@gmail.com?Subject=Hey%20Gregor!" target="_top"> gregorcox@gmail.com</a></p> */}
+
+    <form className="contact-form" action="/action_page.php">
+      <label>Name</label>
+      <input type="text" id="name" name="name" placeholder="Name" />
+
+      <label>Email</label>
+      <input type="email" id="email" name="email" placeholder="Your email" />
+
+      <label>Message</label>
+      <textarea id="message" name="message" placeholder="Your message.."></textarea>
+      <button type="submit" value="Submit" className="button">Submit</button>
+    </form>
+
     <p>or find me on:</p>
     <div className="social-media">
         <a href="https://www.linkedin.com/in/gregorcox/" alt="LinkedIn" target="_blank" rel="noopener noreferrer">
@@ -12,9 +25,6 @@ const Contact = () => (
         <a href="https://www.github.com/gregorcox" alt="GitHub" target="_blank" rel="noopener noreferrer">
           <img src={require('../images/github.png')} height="80" width="80" alt="GitHub"/>
         </a>
-        {/* <a href="https://www.instagram.com" alt="Instagram" target="_blank">
-          <img src={require('../images/instagram.png')} height="80" width="80" alt="Instagram" />
-        </a> */}
       </div>
   </div>
 );
