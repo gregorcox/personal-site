f57893abc6db1fa129b0bdf576c5f114ededfad0
Gregor Cox <gregor@fxdigital.uk>
2020-11-24 16:27:09 +0000

    refactor: remove redundant app file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './containers/Main';
+import './App.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
